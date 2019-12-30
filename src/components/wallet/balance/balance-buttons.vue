335a948cba0ec411254a2c30f50ee91b9c6d2c07
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2019-12-30 14:15:44 +0200

    set password modal

--- src/components/wallet/balance/balance-buttons.vue
+++ src/components/wallet/balance/balance-buttons.vue
@@ -1,40 +1,47 @@
 <template>
 
-    <div class="buttons-row pd-top-20">
-        <div class="btn">
-            <div class="btn-round" v-tooltip.bottom="'Send Money'" >
-                <i class="fa fa-long-arrow-down"></i>
+    <div class="pd-top-20">
+
+        <div class="buttons-row">
+            <div class="btn">
+                <div class="btn-round" v-tooltip.bottom="'Send Money'" >
+                    <i class="fa fa-long-arrow-down"></i>
+                </div>
+            </div>
+
+            <div class="btn">
+                <div class="btn-round" v-tooltip.bottom="'Receive Money'" >
+                    <i class="fa fa-long-arrow-up"></i>
+                </div>
+            </div>
+            <div class="btn">
+                <div class="btn-round"  v-tooltip.bottom="'Scan QR Code'">
+                    <i class="fa fa-qrcode"></i>
+                </div>
             </div>
         </div>
 
-        <div class="btn">
-            <div class="btn-round" v-tooltip.bottom="'Receive Money'" >
-                <i class="fa fa-long-arrow-up"></i>
+        <div class="buttons-row">
+            <div class="btn">
+                <div class="btn-round" @click="openAccountInfoModal" v-tooltip.bottom="'View Account Info'" >
+                    <i class="fa fa-info"></i>
+                </div>
             </div>
-        </div>
-        <div class="btn">
-            <div class="btn-round"  v-tooltip.bottom="'Scan QR Code'">
-                <i class="fa fa-qrcode"></i>
+            <div class="btn" v-if="!encrypted">
+                <div class="btn-round" @click="setPassword" v-tooltip.bottom="'Set Password'" >
+                    <i class="fa fa-unlock-alt"></i>
+                </div>
             </div>
-        </div>
-        <div class="btn">
-            <div class="btn-round" @click="openAccountInfoModal" v-tooltip.bottom="'View Account Info'" >
-                <i class="fa fa-info"></i>
-            </div>
-        </div>
-        <div class="btn" v-if="!encrypted">
-            <div class="btn-round" @click="setPassword" v-tooltip.bottom="'Set Password'" >
-                <i class="fa fa-unlock-alt"></i>
-            </div>
-        </div>
 
-        <div class="btn" v-if="encrypted">
-            <div class="btn-round" @click="removePassword" v-tooltip.bottom="'Remove Password'" >
-                <i class="fa fa-lock"></i>
+            <div class="btn" v-if="encrypted">
+                <div class="btn-round" @click="removePassword" v-tooltip.bottom="'Remove Password'" >
+                    <i class="fa fa-lock"></i>
+                </div>
             </div>
         </div>
 
         <account-info-modal ref="refAccountInfoModal"/>
+        <set-password-modal ref="refSetPasswordModal" />
 
     </div>
 
@@ -42,11 +49,11 @@
 
 <script>
 import AccountInfoModal from "./../account/account-info.modal"
-
+import SetPasswordModal from "./../encryption/set-password.modal"
 
 export default {
 
-    components: {AccountInfoModal},
+    components: { AccountInfoModal, SetPasswordModal },
 
     computed:{
         encrypted(){
@@ -61,7 +68,7 @@ export default {
         },
 
         setPassword(){
-
+            this.$refs.refSetPasswordModal.showModal();
         },
 
         removePassword(){
@@ -76,7 +83,13 @@ export default {
 <style scoped>
 
     .buttons-row{
-        text-align: center;
+        display: inline-block;
+    }
+
+    .buttons-row:first-child{
+    }
+
+    .buttons-row:last-child{
     }
 
     .buttons-row .btn{
---
+++ src/components/wallet/encryption/set-password.modal.vue
@@ -0,0 +1,66 @@
+<template>
+
+    <modal ref="modal" title="Set a password" >
+
+
+        <span class="disabled" >Password</span> <br/>
+        <input type="password" class="label" v-model="password">
+
+        <span class="disabled" >Retype Password</span> <br/>
+        <input type="password" class="label" v-model="retypePassword">
+
+        <input type="submit" value="Set Password" :disabled="password.length === 0 || retypePassword.length === 0" @click="setPassword">
+
+        <span class="thick pd-top-40 pd-bottom-40">Info: Write down your password.</span>
+
+        <div class="centered">
+            <span class="danger">Warning: In case you lose your password, you will need your <strong>seed to recover your seed accounts!</strong>. Imported accounts generated from a different seed can not be recovered by using your seed.</span>
+        </div>
+
+    </modal>
+
+</template>
+
+<script>
+
+import Modal from "src/components/utils/modal"
+
+export default {
+
+    components: {Modal},
+
+    data(){
+        return {
+            password: '',
+            retypePassword: '',
+        }
+    },
+
+    methods: {
+
+        showModal() {
+            this.$refs.modal.showModal();
+        },
+
+        closeModal() {
+            this.$refs.modal.closeModal();
+        },
+
+        setPassword(){
+
+        },
+
+    }
+
+}
+
+</script>
+
+<style scoped>
+    span{
+        display: inline-block
+    }
+    .wordwrap{
+        display: block;
+    }
+</style>
