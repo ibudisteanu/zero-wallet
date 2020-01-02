507c9e51c5233b5751c53dac98de0cd5e9234f54
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2020-01-03 00:14:22 +0200

    table css

--- src/components/explorer/show-blocks.vue
+++ src/components/explorer/show-blocks.vue
@@ -3,24 +3,26 @@
     <div >
 
         <div class="table">
-            <span>Height</span>
-            <span>Hash</span>
-            <span>Kernel Hash</span>
-            <span>Time</span>
-            <span>Size</span>
-            <span>TX</span>
-            <span>Forger</span>
-        </div>
-        <div v-for="blockInfo in blocksInfo" class="table">
+            <div class="table-header table-row">
+                <span>Height</span>
+                <span>Hash</span>
+                <span>Kernel Hash</span>
+                <span>Time</span>
+                <span>Size</span>
+                <span>TX</span>
+                <span>Forger</span>
+            </div>
+            <div v-for="blockInfo in blocksInfo" class="table-row">
 
-            <span>{{blockInfo.height}}</span>
-            <span>{{blockInfo.hash.toString("hex").substr(0, 20)}}</span>
-            <span>{{blockInfo.kernelHash.toString("hex").substr(0, 20)}}</span>
-            <span>{{blockInfo.timestamp}}</span>
-            <span>{{blockInfo.size}}</span>
-            <span>{{blockInfo.txCount}}</span>
-            <span>{{blockInfo.forgedBy.substr(0, 20)}}</span>
+                <span>{{blockInfo.height}}</span>
+                <span>{{blockInfo.hash.toString("hex").substr(0, 20)}}</span>
+                <span>{{blockInfo.kernelHash.toString("hex").substr(0, 20)}}</span>
+                <span>{{blockInfo.timestamp}}</span>
+                <span>{{blockInfo.size}}</span>
+                <span>{{blockInfo.txCount}}</span>
+                <span>{{blockInfo.forgedBy.substr(0, 20)}}</span>
 
+            </div>
         </div>
 
     </div>
@@ -39,7 +41,7 @@ export default {
 
 <style scoped>
 
-    .table{
+    .table-row{
         grid-template-columns: 60px 1fr 1fr 50px 50px 50px 1fr ;
     }
 
