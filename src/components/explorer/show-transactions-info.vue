eaf3ec76e75576bd1f7522eee6a227fbe4e5938a
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2020-01-18 23:58:50 +0200

    fix show transactions info

--- src/components/explorer/show-transactions-info.vue
+++ src/components/explorer/show-transactions-info.vue
@@ -5,7 +5,9 @@
         <div class="table">
             <div class="table-header table-row">
                 <span>Hash</span>
-                <span>nonce</span>
+                <span>Version</span>
+                <span>Script Version</span>
+                <span>Nonce</span>
                 <span>Token</span>
                 <span>Queued</span>
                 <span>In</span>
@@ -14,6 +16,8 @@
             <div v-for="(tx, txId) in transactionsInfo" class="table-row">
 
                 <span><router-link :to="`/explorer/tx/hash/${txId}`">{{txId.substr(0,45)+'...'}}</router-link></span>
+                <span>{{tx.version}}</span>
+                <span>{{tx.scriptVersion}}</span>
                 <span>{{tx.nonce}}</span>
                 <span>{{tx.tokenCurrency.toString("hex")}}</span>
                 <span>{{tx.queued}}</span>
@@ -39,7 +43,7 @@ export default {
 <style scoped>
 
     .table-row{
-        grid-template-columns: 1fr 100px 100px 100px 100px 140px;
+        grid-template-columns: 1fr 60px 60px 100px 100px 140px 60px 60px;
     }
 
 </style>
