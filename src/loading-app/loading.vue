a0c2b2412492fbeb59ee9652d36cce57eaea9b82
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2021-06-02 01:27:03 +0300

    new loading

--- src/loading-app/loading.vue
+++ src/loading-app/loading.vue
@@ -3,41 +3,15 @@
 
         <div class="wrapper center">
 
-            <div class="logo-div">
-                <img :src="require('src/assets/pandora-pay-logo-inline.png').default" class="logo" >
+            <div class="logo-div center">
+                <img :src="require('src/assets/pandora-pay-logo-square.png').default" class="logo" >
             </div>
 
-            <div class="container-flex"  >
-                <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                </ul>
-            </div>
+            <svg width="200px" height="200px" viewBox="0 0 35 35">
+                <polygon class="triangle" fill="none" stroke="#fff" stroke-width="1" points="16,1 32,32 1,32" />
+            </svg>
 
-            <div class="loading-div">
+            <div>
                 <span class="loading-text">
                     <i v-if="isDownloading" class="fas fa-sync fa-spin"></i>
                     {{progressStatus}}
@@ -92,7 +66,7 @@ export default {
                     fetchProgress({
                         // implement onProgress method
                         onProgress(progress) {
-                            self.progressStatus = `WASM:  ${(progress.transferred/1024/1024).toFixed(2)}mb / ${(progress.total/1024/1024).toFixed(2)}mb... eta: ${(progress.eta/1000000000).toFixed(0)}s`
+                            self.progressStatus = `WASM:  ${(progress.transferred/1024/1024).toFixed(2)}mb / ${(progress.total/1024/1024).toFixed(2)}mb`
                         },
                     })
                 ).then((r)=> {
@@ -128,17 +102,15 @@ export default {
 
 <style scoped>
 
-    .logo{
-        width: 100%;
-        max-width: 200px;
-    }
-
     .logo-div{
-        padding-bottom: 20px;
+        margin-top: 20px;
+        margin-left: 50px;
     }
 
-    .loading-div{
-        padding-top: 40px;
+    .logo{
+        max-width: 60px;
+        margin-top: 30px;
+        margin-left: -20px;
     }
 
     .loading-text{
@@ -157,203 +129,36 @@ export default {
         transform: translate(-50%, -50%);
     }
 
-    .container-flex {
+    body {
+        background: white;
         display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 100vh;
     }
 
-    .wrapper {
-        width: auto;
-        height: auto;
+    .triangle {
+        stroke:#0EB4DB;
+        stroke-dasharray: 17;
+        animation: dash 2.5s cubic-bezier(0.35, 0.04, 0.63, 0.95) infinite;
     }
-    ul {
-        display: grid;
-        grid-template-columns: repeat(5, 1fr);
-        animation: rot 16s linear infinite;
-        list-style-type: none;
-    }
-    @keyframes rot {
-        100% {
-            transform: rotate(360deg);
+
+    @keyframes dash {
+        to {
+            stroke-dashoffset: 136;
         }
     }
-    li {
-        width: 40px;
-        height: 40px;
-        background: #277bff;
-        border-radius: 4px;
-        box-shadow: 0 0 1px #fff, 0 0 5px #000000, 0 0 10px #bd9fff, 0 0 15px #bd9fff, 0 0 25px #bd9fff, 0 0 55px #bd9fff;
-        animation: scale 0.8s linear alternate infinite;
+
+    .loading {
+        font-family: 'Orbitron', sans-serif;
+        font-size: 5px;
+        animation: blink .9s ease-in-out infinite;
     }
-    @keyframes scale {
-        100% {
-            transform: scale(0.1);
+
+    @keyframes blink {
+        50% {
             opacity: 0;
         }
-    }
-    li:nth-child(1) {
-        z-index: 24;
-    }
-    li:nth-child(2) {
-        z-index: 23;
-    }
-    li:nth-child(3) {
-        z-index: 22;
-    }
-    li:nth-child(4) {
-        z-index: 21;
-    }
-    li:nth-child(5) {
-        z-index: 20;
-    }
-    li:nth-child(6) {
-        z-index: 19;
-    }
-    li:nth-child(7) {
-        z-index: 18;
-    }
-    li:nth-child(8) {
-        z-index: 17;
-    }
-    li:nth-child(9) {
-        z-index: 16;
-    }
-    li:nth-child(10) {
-        z-index: 15;
-    }
-    li:nth-child(11) {
-        z-index: 14;
-    }
-    li:nth-child(12) {
-        z-index: 13;
-    }
-    li:nth-child(13) {
-        z-index: 12;
-    }
-    li:nth-child(14) {
-        z-index: 11;
-    }
-    li:nth-child(15) {
-        z-index: 10;
-    }
-    li:nth-child(16) {
-        z-index: 9;
-    }
-    li:nth-child(17) {
-        z-index: 8;
-    }
-    li:nth-child(18) {
-        z-index: 7;
-    }
-    li:nth-child(19) {
-        z-index: 6;
-    }
-    li:nth-child(20) {
-        z-index: 5;
-    }
-    li:nth-child(21) {
-        z-index: 4;
-    }
-    li:nth-child(22) {
-        z-index: 3;
-    }
-    li:nth-child(23) {
-        z-index: 2;
-    }
-    li:nth-child(24) {
-        z-index: 1;
-    }
-    li:nth-child(25) {
-        z-index: 0;
-    }
-    li:nth-child(1) {
-        animation-delay: 0.1s;
-    }
-    li:nth-child(7) {
-        animation-delay: 0.3s;
-    }
-    li:nth-child(13) {
-        animation-delay: 0.5s;
-    }
-    li:nth-child(19) {
-        animation-delay: 0.7s;
-    }
-    li:nth-child(24) {
-        animation-delay: 0.9s;
-    }
-    li:nth-child(2) {
-        animation-delay: 0.2s;
-    }
-    li:nth-child(8) {
-        animation-delay: 0.4s;
-    }
-    li:nth-child(14) {
-        animation-delay: 0.6s;
-    }
-    li:nth-child(20) {
-        animation-delay: 0.8s;
-    }
-    li:nth-child(3) {
-        animation-delay: 0.3s;
-    }
-    li:nth-child(9) {
-        animation-delay: 0.5s;
-    }
-    li:nth-child(15) {
-        animation-delay: 0.7s;
-    }
-    li:nth-child(4) {
-        animation-delay: 0.4s;
-    }
-    li:nth-child(10) {
-        animation-delay: 0.6s;
-    }
-    li:nth-child(5) {
-        animation-delay: 0.5s;
-    }
-    li:nth-child(1) {
-        animation-delay: 0.1s;
-    }
-    li:nth-child(6) {
-        animation-delay: 0.2s;
-    }
-    li:nth-child(11) {
-        animation-delay: 0.3s;
-    }
-    li:nth-child(16) {
-        animation-delay: 0.4s;
-    }
-    li:nth-child(21) {
-        animation-delay: 0.5s;
-    }
-    li:nth-child(7) {
-        animation-delay: 0.3s;
-    }
-    li:nth-child(12) {
-        animation-delay: 0.4s;
-    }
-    li:nth-child(17) {
-        animation-delay: 0.5s;
-    }
-    li:nth-child(22) {
-        animation-delay: 0.6s;
-    }
-    li:nth-child(13) {
-        animation-delay: 0.5s;
-    }
-    li:nth-child(18) {
-        animation-delay: 0.6s;
-    }
-    li:nth-child(23) {
-        animation-delay: 0.7s;
-    }
-    li:nth-child(19) {
-        animation-delay: 0.7s;
-    }
-    li:nth-child(24) {
-        animation-delay: 0.8s;
-    }
-    li:nth-child(25) {
-        animation-delay: 0.9s;
     }
 
 </style>
