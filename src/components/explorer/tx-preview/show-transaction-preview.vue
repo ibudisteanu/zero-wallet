be4eb216bd62225d38d95cf62b9fccb07c99bd2b
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2021-10-09 23:24:57 +0300

    minor fixes

--- src/components/explorer/tx-preview/show-transaction-preview.vue
+++ src/components/explorer/tx-preview/show-transaction-preview.vue
@@ -8,20 +8,20 @@
         </template>
         <template v-else>
 
-            <span class="col-12 d-block d-sm-none text-dark">Hash</span>
-            <span class="col-12 col-sm-2 col-md-2 text-truncate">
+            <span class="col-4 d-block d-sm-none text-dark text-truncate">Hash</span>
+            <span class="col-8 col-sm-2 col-md-2 text-truncate">
                 <router-link :to="`/explorer/tx/${txHash}`">
                     {{tx.hash}}
                 </router-link>
            </span>
 
-            <span class="col-12 d-block d-sm-none text-dark">Type</span>
-            <div class="col-12 col-sm-2 col-md-1 text-truncate">
+            <span class="col-4 d-block d-sm-none text-dark text-truncate">Type</span>
+            <div class="col-8 col-sm-2 col-md-1 text-truncate">
                 <span :class="`badge badge-soft-${badgeColor}`" v-tooltip.bottom="getTxScriptText">{{getTxScriptTextShort}}</span>
             </div>
 
-            <span class="col-12 d-block d-sm-none text-dark">Time</span>
-            <div class="col-12 col-sm-2 col-md-1 text-truncate">
+            <span class="col-4 d-block d-sm-none text-dark text-truncate">Time</span>
+            <div class="col-8 col-sm-2 col-md-1 text-truncate">
                 <template v-if="txInfo">
                     <span v-if="txInfo.mempool">
                         pending
@@ -32,8 +32,8 @@
                 </template>
             </div>
 
-            <span class="col-12 d-block d-sm-none text-dark">Confirmations</span>
-            <div class="col-12 col-sm-1 col-md-1 text-truncate">
+            <span class="col-4 d-block d-sm-none text-dark text-truncate">Confirmations</span>
+            <div class="col-8 col-sm-1 col-md-1 text-truncate">
                 <template v-if="txInfo">
                     <span v-if="txInfo.mempool">
                         pending
@@ -46,8 +46,8 @@
                 </template>
             </div>
 
-            <span class="col-12 d-block d-sm-none text-dark">Data</span>
-            <span class="col-12 col-sm-5 col-md-7">
+            <span class="col-4 d-block d-sm-none text-dark text-truncate">Data</span>
+            <span class="col-8 col-sm-5 col-md-7">
                 <show-transaction-preview-data :tx="tx" />
             </span>
 
